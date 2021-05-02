8c523e698dd6050ef950760a4eb88b46770c2958
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-05-01 23:59:13 -0700

    Implement Sort class

---
+++ sort/build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([1000, -1, 50, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(sorter.collection);
+// console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection('fraegaergohaz');
+var sorter = new Sorter_1.Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
