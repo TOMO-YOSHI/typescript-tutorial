21de46af7f0d660761bf9640990555d43723d1b7
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-05-12 23:37:16 -0700

    create decorators dir

--- express/src/index.ts
+++ express/src/index.ts
@@ -12,3 +12,23 @@ app.use(router);
 app.listen(3030, ()=>{
     console.log(`Lisining on port 3030`);
 });
+
+// Do Not Write
+
+// class Server {
+//     app: express.Express = express();
+
+//     constructor() {
+//         this.app.use(bodyParser.urlencoded({ extended: true }));
+//         this.app.use(cookieSession({ keys: ['fragraegr'] }))
+//         this.app.use(router);
+//     }
+
+//     start(): void {
+//         app.listen(3030, () => {
+//             console.log(`Lisining on port 3030`);
+//         });
+//     }
+// }
+
+// new Server().start();
